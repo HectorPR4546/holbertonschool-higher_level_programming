0556a15dfde2059bf9b8c263e133808aab4dbef7
HectorPR4546 <h_perez93@yahoo.com>
2025-05-10 15:43:41 -0400

    Task 2 fix

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,19 +2,12 @@
 import sys
 
 if __name__ == "__main__":
-    args = sys.argv[1:]  # Get the list of arguments (excluding the script name)
+    args = sys.argv[1:]
+    num_args = len(args)
 
-    num_args = len(args)  # Get the number of arguments
-
-    # Print the number of arguments
-    print(f"{num_args} argument{'s' if num_args != 1 else ''}:", end="")
-
-    # Print a dot if no arguments were passed
     if num_args == 0:
-        print(".")
+        print("0 arguments.")
     else:
-        print()  # Print a new line
-
-        # Print each argument
+        print(f"{num_args} argument{'s' if num_args != 1 else ''}:")
         for i, arg in enumerate(args, start=1):
-            print(f"{i}: {arg}")  # Print the position and value of each argument
+            print(f"{i}: {arg}")
