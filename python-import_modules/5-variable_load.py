a6b94b8db3958daf038c858b156a344ef5140797
HectorPR4546 <h_perez93@yahoo.com>
2025-05-10 17:15:00 -0400

    Task 5 documentation

--- python-import_modules/5-variable_load.py
+++ python-import_modules/5-variable_load.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This script imports the variable 'a' from the module 'variable_load_5'
+and prints its value.
+
+The script is meant to be executed directly and not imported.
+"""
 
 from variable_load_5 import a
 
