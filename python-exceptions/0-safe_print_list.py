5a8ce1b7e1789f2753ec67d147e52c4973c7ace2
HectorPR4546 <h_perez93@yahoo.com>
2025-05-16 14:33:44 -0400

    Task 0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    """
+    Print x elements of a list safely.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): Number of elements to attempt to print.
+
+    Returns:
+        int: The number of elements actually printed.
+    """
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    finally:
+        print()
+        return count
