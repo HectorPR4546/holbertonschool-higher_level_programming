8d5118d9a62554c4f3e2a91a479dd686c97f535d
HectorPR4546 <h_perez93@yahoo.com>
2025-05-16 13:48:18 -0400

    Task 11 fix for 3 lines, (4) new line

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,18 +1,3 @@
 #!/usr/bin/python3
-"""
-Module that provides a function to multiply all elements of a list by a number.
-"""
-
-
 def multiply_list_map(my_list=[], number=0):
-    """
-    Multiply all elements of a list by a given number using map.
-
-    Args:
-        my_list (list): A list of integers.
-        number (int): The number to multiply each element by.
-
-    Returns:
-        list: A new list with all elements multiplied by the given number.
-    """
     return list(map(lambda x: x * number, my_list))
