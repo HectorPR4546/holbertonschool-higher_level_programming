0074f9c17f3e8c74d46bad30e8f55ffd405f91bc
HectorPR4546 <h_perez93@yahoo.com>
2025-05-17 09:48:47 -0400

    Task 2 Fix

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    """Print the first x elements of my_list that are integers.
-    Use "{:d}".format() to print each integer without a newline,
-    then print a newline at the end.
-    Return the number of integers printed.
-    """
     count = 0
     for i in range(x):
         try:
