265c9b5dd3e123898cc04904522c6e254774149c
HectorPR4546 <h_perez93@yahoo.com>
2025-05-18 12:00:23 -0400

    Fix

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -13,6 +13,9 @@ def print_square(size):
         ValueError: If size is less than 0.
     """
 
+    err = "unsupported operand type(s) for +: 'NoneType' and 'int'"
+    if size is None:
+        raise TypeError(err)
     if type(size) is not int:
         raise TypeError("size must be an integer")
     if size < 0:
