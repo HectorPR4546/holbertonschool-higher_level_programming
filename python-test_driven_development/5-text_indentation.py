f6798786f9dd59252f418e1d76c8152a796e82b3
HectorPR4546 <h_perez93@yahoo.com>
2025-05-18 12:22:51 -0400

    Task 4

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,23 @@
+def text_indentation(text):
+    """
+    Prints text with 2 new lines after each '.', '?', and ':' character.
+
+    Args:
+        text (str): The input text to format.
+
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    i = 0
+    while i < len(text):
+        print(text[i], end="")
+        if text[i] in ".?:":
+            print("\n")
+            i += 1
+            while i < len(text) and text[i] == " ":
+                i += 1
+            continue
+        i += 1
