ca3af1afffbcca5a10506cd3367048a52fc9ffdc
HectorPR4546 <h_perez93@yahoo.com>
2025-05-20 07:46:17 -0400

    Fix 25

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,11 +1,25 @@
-# !/usr/bin/python3
-"""
-This module defines an empty class Square.
-"""
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""0-square module.
 
+This module defines an empty Square class for educational purposes.
+
+Classes:
+    Square: An empty class that defines a square.
+
+Example:
+    >>> my_square = Square()
+    >>> print(type(my_square))
+    <class '0-square.Square'>
+    >>> print(my_square.__dict__)
+    {}
+
+"""
 
 class Square:
-    """
-    An empty class that defines a square.
+    """An empty class that defines a square.
+
+    This class does not define any attributes or methods yet.
+
     """
     pass
