f2d12cadf2eb3a952a263e1b633995839a7e0b12
HectorPR4546 <h_perez93@yahoo.com>
2025-05-20 03:02:19 -0400

    Fix 16

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 class Square:
-    """An empty class that defines a square.
-
-    This class does not define any attributes or methods yet.
-
-    """
-    {}
+    """An empty class that defines a square."""
+    pass
