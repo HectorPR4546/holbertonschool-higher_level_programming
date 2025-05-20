9b74580c3ed069261bf68245fa410301b76065bb
HectorPR4546 <h_perez93@yahoo.com>
2025-05-20 02:33:43 -0400

    Fix 6

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -1,21 +1,5 @@
 #!/usr/bin/python3
-"""Main module for testing the Square class.
-
-This script demonstrates instantiating the Square class defined in
-`0-square.py` and printing its type and attributes.
-
-Example:
-    $ ./0-main.py
-    <class '0-square.Square'>
-    {}
-
-Attributes:
-    None
-
-"""
-
 Square = __import__('0-square').Square
-
 my_square = Square()
-print(type(my_square))
-print(my_square.__dict__)
+print(type(my_square))        # Output: <class '0-square.Square'>
+print(my_square.__dict__)     # Output: {}
--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,26 +1,6 @@
 #!/usr/bin/python3
-"""0-square module.
-
-This module defines an empty Square class as a starting point
-for learning about classes in Python.
-
-Classes:
-    Square: Represents a geometric square with no attributes or methods yet.
-
-Example:
-    To use this module, simply import and instantiate the Square class:
-
-        from 0-square import Square
-        my_square = Square()
-"""
+"""A module that defines an empty Square class."""
 
 class Square:
-    """An empty class that defines a square.
-
-    This class currently does not include any attributes or methods.
-
-    Note:
-        This is part of Holberton School's Python OOP introduction.
-
-    """
+    """An empty class that represents a square."""
     pass
