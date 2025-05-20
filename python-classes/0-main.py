05b9066cb001f589bd682ce994708ff257a79612
HectorPR4546 <h_perez93@yahoo.com>
2025-05-20 02:47:15 -0400

    Fix 9

--- python-classes/0-main.py
+++ python-classes/0-main.py
@@ -1,4 +1,20 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Main module for testing the Square class.
+
+This script demonstrates instantiating the Square class defined in
+`0-square.py` and printing its type and attributes.
+
+Example:
+    $ ./0-main.py
+    <class '0-square.Square'>
+    {}
+
+Attributes:
+    None
+
+"""
+
 Square = __import__('0-square').Square
 
 my_square = Square()
