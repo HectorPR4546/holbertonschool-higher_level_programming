433940de102dc5197b0aed97e79026be25ad58de
HectorPR4546 <h_perez93@yahoo.com>
2025-05-23 11:11:29 -0400

    Task 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Defines an empty Rectangle class.
+
+This module defines a basic class Rectangle to be expanded later.
+"""
+
+
+class Rectangle:
+    """Represents a rectangle.
+
+    This class is currently empty and will be expanded in later tasks.
+    """
+    pass
