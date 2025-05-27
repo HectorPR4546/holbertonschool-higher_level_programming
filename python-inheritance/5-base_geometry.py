54e03d107d5c08d8ee9c1741f9360743e09fb770
HectorPR4546 <h_perez93@yahoo.com>
2025-05-27 17:17:10 -0400

    Task 5

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Defines an empty class BaseGeometry."""
+
+
+class BaseGeometry:
+    """An empty class that will be expanded in later tasks."""
+    pass
