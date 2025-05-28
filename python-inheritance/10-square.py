ac2cd371a965cdb6d8581df487896366fd67678d
HectorPR4546 <h_perez93@yahoo.com>
2025-05-28 13:00:09 -0400

    Task 10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""Defines a Square class inheriting from Rectangle."""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a square, inheriting from Rectangle."""
+
+    def __init__(self, size):
+        """Initializes a square with a given size.
+
+        Args:
+            size (int): The size of the square sides.
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square.
+
+        Returns:
+            int: The area of the square.
+        """
+        return self.__size * self.__size
