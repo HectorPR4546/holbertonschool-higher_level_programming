5b989a3b40e31a80675e86497f3aeacb69fb40f5
HectorPR4546 <h_perez93@yahoo.com>
2025-06-05 12:39:25 -0400

    Task 0 Fix 2

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,6 +1,4 @@
-#!/usr/bin/python3
-
 def read_file(filename=""):
-
-    with open(filename, mode="r", encoding="utf-8") as file:
-        print(file.read(), end="")
+    """Reads a text file (UTF8) and prints it to stdout."""
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
