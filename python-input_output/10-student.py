f9ce1a976e9a9f965a2a5ad816a5acd333e31f01
HectorPR4546 <h_perez93@yahoo.com>
2025-06-06 12:49:25 -0400

    Task 10

---
+++ python-input_output/10-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Defines a Student class with filtered JSON representation."""
+
+
+class Student:
+    """Represents a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student.
+
+        Args:
+            first_name (str): The first name of the student.
+            last_name (str): The last name of the student.
+            age (int): The age of the student.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of the Student.
+
+        Args:
+            attrs (list): A list of strings representing attribute names to include.
+
+        Returns:
+            dict: A dictionary containing the specified student's attributes.
+        """
+        if attrs is None:
+            return self.__dict__
+        return {key: getattr(self, key) for key in attrs if hasattr(self, key)}
