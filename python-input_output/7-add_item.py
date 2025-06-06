a1d4c6c94d9657ac8d3560252c18ced42342dabc
HectorPR4546 <h_perez93@yahoo.com>
2025-06-06 12:05:25 -0400

    Task 7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Script to add all command-line arguments to a list and save them to a JSON file."""
+
+import sys
+from os.path import exists
+from 5-save_to_json_file import save_to_json_file
+from 6-load_from_json_file import load_from_json_file
+
+def main():
+    """Main function to handle the script logic."""
+    filename = "add_item.json"
+    try:
+        items = load_from_json_file(filename)
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, filename)
+
+if __name__ == "__main__":
+    main()
