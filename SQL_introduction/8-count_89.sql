91efc1e571d60f53164cfeeca10b01027f1bfb76
HectorPR4546 <h_perez93@yahoo.com>
2025-06-28 17:34:28 -0400

    Task 8: Count records with id = 89

---
+++ SQL_introduction/8-count_89.sql
@@ -0,0 +1,3 @@
+-- This script displays the number of records with id = 89 in the table 'first_table'.
+-- As a Holberton student, I'm learning to query specific data counts.
+SELECT COUNT(*) FROM first_table WHERE id = 89;
