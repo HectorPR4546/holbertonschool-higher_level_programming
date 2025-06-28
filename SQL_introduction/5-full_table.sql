8b2eca81f327bca3ef8dc75e8350def0bce9f5b4
HectorPR4546 <h_perez93@yahoo.com>
2025-06-28 17:29:53 -0400

    Task 5: Show full table description

---
+++ SQL_introduction/5-full_table.sql
@@ -0,0 +1,3 @@
+-- This script prints the full description of the table 'first_table' from the current database.
+-- As a Holberton student, I'm learning different ways to inspect table schemas.
+SHOW CREATE TABLE first_table;
