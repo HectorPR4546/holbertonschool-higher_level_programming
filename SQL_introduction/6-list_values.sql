280acb59c931513240e7d1869d835bd8a8a1dea2
HectorPR4546 <h_perez93@yahoo.com>
2025-06-28 17:31:32 -0400

    Task 6: List all rows from first_table

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,3 @@
+-- This script lists all rows of the table 'first_table' from the current database.
+-- As a Holberton student, I'm learning to retrieve data from tables.
+SELECT * FROM first_table;
