851c6b237e51e1297671ecd3d4b24779d2561744
HectorPR4546 <h_perez93@yahoo.com>
2025-06-28 17:32:36 -0400

    Task 7: Insert new row into first_table

---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- This script inserts a new row into the table 'first_table' in the current database.
+-- As a Holberton student, I'm learning to add data to tables.
+INSERT INTO first_table (id, name) VALUES (89, "Best School");
