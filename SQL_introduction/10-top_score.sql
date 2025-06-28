1341c5fa67212288bded19254b51e99266456cf3
HectorPR4546 <h_perez93@yahoo.com>
2025-06-28 17:37:28 -0400

    Task 10: List records by best score

---
+++ SQL_introduction/10-top_score.sql
@@ -0,0 +1,3 @@
+-- This script lists all records of the table 'second_table', ordered by score (top first).
+-- As a Holberton student, I'm learning to sort and display data.
+SELECT score, name FROM second_table ORDER BY score DESC;
