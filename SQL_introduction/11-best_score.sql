7c7be45cf56fb13d345d1b1329f4ea5d8fe5349f
HectorPR4546 <h_perez93@yahoo.com>
2025-06-28 17:38:27 -0400

    Task 11: Select records with score >= 10

---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- This script lists all records with a score >= 10 in the table 'second_table', ordered by score.
+-- As a Holberton student, I'm learning to filter and order data based on conditions.
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
