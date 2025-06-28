3c7eeae56d76af772d6b3ad5285efa002a22df5c
HectorPR4546 <h_perez93@yahoo.com>
2025-06-28 19:27:38 -0400

    Refactor: Update 0-select_states.py and 1-filter_states.py for pycodestyle and documentation compliance

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Lists all states with a name starting with N from the database hbtn_0e_0_usa """
+""" Lists states starting with 'N' from the database hbtn_0e_0_usa. """
 import MySQLdb
 import sys
 
@@ -9,7 +9,9 @@ if __name__ == "__main__":
     db_name = sys.argv[3]
 
     db = MySQLdb.connect(host="localhost", port=3306,
-                           user=username, passwd=password, db=db_name)
+                           user=username,
+                           passwd=password,
+                           db=db_name)
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
