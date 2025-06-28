cc7b344350e2c4f2758a3fba46a91d0017462d88
HectorPR4546 <h_perez93@yahoo.com>
2025-06-28 17:41:03 -0400

    Task 13: Remove records with score <= 5

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- This script removes all records with a score <= 5 from the table 'second_table'.
+-- As a Holberton student, I'm learning to delete specific data based on conditions.
+DELETE FROM second_table WHERE score <= 5;
