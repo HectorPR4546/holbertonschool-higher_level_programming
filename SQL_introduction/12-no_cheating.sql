2ce7c1e8147fb7daf1494f5f9497a02b78e7c95c
HectorPR4546 <h_perez93@yahoo.com>
2025-06-28 17:39:24 -0400

    Task 12: Update Bob's score

---
+++ SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,3 @@
+-- This script updates the score of Bob to 10 in the table 'second_table'.
+-- As a Holberton student, I'm learning to update specific records based on conditions.
+UPDATE second_table SET score = 10 WHERE name = "Bob";
