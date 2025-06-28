af87fba9d596d515fef63ae7113cf926dea12fc6
HectorPR4546 <h_perez93@yahoo.com>
2025-06-28 17:28:36 -0400

    Task 4: Create first_table

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- This script creates a table called 'first_table' in the current database.
+-- As a Holberton student, I'm learning to define table structures.
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
