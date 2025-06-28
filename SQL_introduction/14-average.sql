9f5aa9304a054024157a6e6cb92bcef9cf254254
HectorPR4546 <h_perez93@yahoo.com>
2025-06-28 17:42:01 -0400

    Task 14: Compute average score

---
+++ SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- This script computes the average score of all records in the table 'second_table'.
+-- As a Holberton student, I'm learning to perform aggregate calculations on data.
+SELECT AVG(score) AS average FROM second_table;
