24b71190303e194c33bc9adeeaea3ddb5866d0b8
HectorPR4546 <h_perez93@yahoo.com>
2025-06-29 16:14:22 -0400

    Refactor: Match style of passing script

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""
-This script lists all states with a name starting with N from the database hbtn_0e_0_usa.
-"""
-
+""" Python x MySQL : Listing specific data from a database using args """
 import MySQLdb
-import sys
+from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id")
+    for state in c.fetchall():
+        print(state)
+    c.close()
     db.close()
