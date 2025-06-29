e278318df5a794394dc26c87636c5bfe3d071d67
HectorPR4546 <h_perez93@yahoo.com>
2025-06-29 18:12:23 -0400

    Fix: Overwrite with exact working code for Task 14

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,30 +1,18 @@
 #!/usr/bin/python3
-"""
-Lists all City objects from the database hbtn_0e_14_usa.
-"""
-
+""" prints the State object w/ name passed as argument from db """
 import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
 
-if __name__ == '__main__':
 
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    engine = create_engine(
-        f'mysql+mysqldb://{username}:{password}@localhost:3306/{database}'
-        )
-
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-
-    for city, state in session.query(City, State)
-                               .filter(City.state_id == State.id)
-                               .order_by(City.id).all():
-        print(f'{state.name}: ({city.id}) {city.name}')
-
-    session.close()
+    for instance in (session.query(State.name, City.id, City.name)
+                     .filter(State.id == City.state_id)):
+        print(instance[0] + ": (" + str(instance[1]) + ") " + instance[2])
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-"""
-Defines the City class for a MySQL database.
-"""
+"""Definition of the City class"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base
 
 
 class City(Base):
-    """
-    Represents a city for a MySQL database.
-
-    Attributes:
-        __tablename__ (str): The name of the MySQL table to link to.
-        id (sqlalchemy.Integer): The city's id.
-        name (sqlalchemy.String): The city's name.
-        state_id (sqlalchemy.Integer): The state id the city belongs to.
-    """
+    """City class that represents the `cities` table"""
     __tablename__ = 'cities'
+
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
