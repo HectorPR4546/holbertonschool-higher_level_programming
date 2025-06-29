9b7adaf410ef4baffe9b6ed44825a336f89da38c
HectorPR4546 <h_perez93@yahoo.com>
2025-06-29 17:47:43 -0400

    Fix: Overwrite with exact working code for Task 8

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,27 +1,34 @@
 #!/usr/bin/python3
-""" Python x SQLAlchemy : Prints the first State object from the database """
-
-import sys
-from model_state import Base, State
+"""
+Write a script that prints
+the first State object from the database hbtn_0e_6_usa
+"""
+from sys import argv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import State
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+
+    # Collect data from argv arguments
+    username = argv[1]
+    passwd = argv[2]
+    db = argv[3]
+
+    # Make connection
     engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
+        f'mysql+mysqldb://{username}:{passwd}@localhost:3306/{db}'
+        )
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    # Make 'cursor'
+    session = sessionmaker(bind=engine)
+    session = session()
 
-    first_state = session.query(State).order_by(State.id).first()
+    # Fetch only first row
+    states = session.query(State).first()
 
-    if first_state:
-        print("{}: {}".format(first_state.id, first_state.name))
+    if states:
+        print(f"1: {states.name}")
     else:
         print("Nothing")
 
