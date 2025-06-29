9d5e15d8d36ad2990cac9967160c503455118fc1
HectorPR4546 <h_perez93@yahoo.com>
2025-06-29 16:59:19 -0400

    Fix: Split SQL query line for pycodestyle compliance

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
                          passwd=argv[2],
                          db=argv[3])
     c = db.cursor()
-    c.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
+    c.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states             ON cities.state_id = states.id ORDER BY cities.id ASC")
     for row in c.fetchall():
         print(row)
     c.close()
